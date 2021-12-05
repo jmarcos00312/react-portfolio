dc1ff19488f2beec547e9760c806465937674084
Jeremiah <jeremiahmarcos27@outlook.com>
2021-12-05 15:05:12 -0600

    added the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Navbar from './component/Navbar.js'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Navbar />
 
-    </div>
+    </Router>
   );
 }
 

b0b5243eb260558e2f3916ab55bfe51609063468
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-01-31 14:45:25 -0600

    almost done styling contact me component

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -8,25 +8,29 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 function Contact() {
     return (
         <div className="contact-container">
-            <div className="icons">
-                <Link href="https://www.linkedin.com/in/jeremiahmarcos/" target="_blank" rel="noreferrer noopener">
-                    <LinkedInIcon />
-                </Link>
-                <Link href="https://github.com/jmarcos00312" target="_blank" rel="noreferrer noopener">
-                    <GitHubIcon />
-                </Link>
+            <div>
+                <div className="icons">
+                    <h1 href="https://www.linkedin.com/in/jeremiahmarcos/" target="_blank" rel="noreferrer noopener" >
+                        <LinkedInIcon fontSize="xx-large" />
+                    </h1>
+                    <hr />
+                    <h1 href="https://github.com/jmarcos00312" target="_blank" rel="noreferrer noopener" >
+                        <GitHubIcon fontSize="xx-large" />
+                    </h1>
+                </div>
             </div>
             <div className="contact-form">
-                <form action="https://formspree.io/f/xaylvewk">
+                <form action="https://formspree.io/f/xaylvewk" >
                     <div className="form">
-                    <label>Name</label>
-                    <input id="name" name="name" required type="text" placeholder="Enter name" />
-                    <label>Email: </label>
-                    <input id="email" name="email" required type="email" placeholder="Enter email" />
-                    <label>Message: </label>
-                    <textarea id="content" name="content"
-                        cols="20" rows="7" placeholder="Like my page? Disliked it? Let me know."></textarea>
-                        </div>
+                        <label>Name</label>
+                        <input id="name" name="name" required type="text" placeholder="Enter name" className="form-name-email" />
+                        <label>Email: </label>
+                        <input id="email" name="email" required type="email" placeholder="Enter email" className="form-name-email" />
+                        <label>Message: </label>
+                        <textarea id="content" name="content"
+                            cols="20" rows="7" placeholder="Do you have any question?"></textarea>
+                        <button type="submit">Submit</button>
+                    </div>
                 </form>
             </div>
         </div>
